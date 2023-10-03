e0b47fcafda9153741d2c78e12aaef0575203ff7
Euphrosine <euphrosine@46.com>
2023-10-03 10:36:10 +0200

    external database added

--- schair_project/schair_project/settings.py
+++ schair_project/schair_project/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'schair_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'lM6lwTddRkeffbHBlzPM',
+        'HOST': 'containers-us-west-59.railway.app',
+        'PORT': '6206',
+
     }
 }
 
