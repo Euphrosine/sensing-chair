0f93d30bc336c7bf9fb20cce672ecd04b5d752a4
Euphrosine <euphrosine@46.com>
2023-10-10 12:23:09 +0300

    missing module added

--- schair_app/urls.py
+++ schair_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('schair_data_api/', views.schair_data_api, name='schair_data_api'),
-    path('schair_data_view/', views.schair_data_view, name='schair_data_view'),
+    path('', views.schair_data_view, name='schair_data_view'),
 ]
--- schair_project/urls.py
+++ schair_project/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('schair/', include('schair_app.urls')),  
+    path('', include('schair_app.urls')),  
 
 ]
